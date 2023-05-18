4f4be77c0d495599024ba2488329858b4c06bcd8
gopikapade <kapadegopi.gk2022@gmail.com>
2023-05-18 23:46:18 +0530

    Login Page And Enquiry Page is created And Push

--- HomeLoanAppUI/src/app/app-routing.module.ts
+++ HomeLoanAppUI/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './templates/landing-page/landing-page.component';
+import { EnquiryFormComponent } from './templates/enquiry-form/enquiry-form.component';
+import { LoginComponent } from './templates/login/login.component';
 
 const routes: Routes = [
-  {path:'', component:LandingPageComponent}
+  {path:'', component:LandingPageComponent },
+{path:'enquiry',component:EnquiryFormComponent},
+{path:'login',component:LoginComponent}
+
 ];
 
 @NgModule({
