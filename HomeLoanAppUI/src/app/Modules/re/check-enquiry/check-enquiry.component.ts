dab307a14421fb522528fd011925df0b56b4c38f
Bhushan <80579471+lurnfx@users.noreply.github.com>
2023-05-20 00:28:26 +0530

    Added Enqury And Forword To oe

--- HomeLoanAppUI/src/app/Modules/re/check-enquiry/check-enquiry.component.ts
+++ HomeLoanAppUI/src/app/Modules/re/check-enquiry/check-enquiry.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Enquiry } from 'src/app/model/enquiry';
+import { EnquiryService } from 'src/app/service/enquiry.service';
 
 @Component({
   selector: 'app-check-enquiry',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./check-enquiry.component.css']
 })
 export class CheckEnquiryComponent {
+
+  constructor(private enquiryServices:EnquiryService){}
+
+  enquries:Array<Enquiry>=[]
+
+  ngOnInit(){
+
+       this.enquiryServices.getEnqiries().subscribe((data:any)=>{
+              this.enquries=data.body;
+       })
+     }
+
+    forwrdToOe(id:number, enquiry:Enquiry){
+
+        this.enquiryServices.oeForward(id, enquiry).subscribe((data:any)=>{console.log(data)})
+     }
 
 }
--- HomeLoanAppUI/src/app/service/enquiry.service.ts
+++ HomeLoanAppUI/src/app/service/enquiry.service.ts
@@ -11,6 +11,15 @@ export class EnquiryService {
 
   saveenquiry(enquiryForm:FormGroup){
 
-    return this.http.post("http://localhost:7575/enquiry/addenquiry",enquiryForm.value)
+    return this.http.post("http://localhost:8080/enquiry/addenquiry", enquiryForm.value)
   }
+
+  getEnqiries(){
+     return this.http.get("http://localhost:8080/enquiry/getenquiry")
+  }
+
+  oeForward(id, enquiry){
+    return   this.http.put("http://localhost:8080/enquiry/oeForward/"+id, enquiry)
+  }
+
 }
