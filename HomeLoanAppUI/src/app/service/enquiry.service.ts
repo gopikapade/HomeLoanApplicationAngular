ad60690555e99db9e1fd3c578935bbd9b98baaea
Bhushan <80579471+lurnfx@users.noreply.github.com>
2023-05-20 11:16:53 +0530

    Service changes
    push

--- HomeLoanAppUI/src/app/service/enquiry.service.ts
+++ HomeLoanAppUI/src/app/service/enquiry.service.ts
@@ -10,7 +10,21 @@ export class EnquiryService {
   constructor(private http:HttpClient) { }
 
   saveenquiry(enquiryForm:FormGroup){
-
-    return this.http.post("http://localhost:8080/enquiry/addenquiry",enquiryForm.value)
+   return this.http.post("http://localhost:8080/enquiry/addenquiry", enquiryForm.value)
   }
+
+
+  getEnqiries(){
+     return this.http.get("http://localhost:8080/enquiry/getenquiry")
+  }
+
+
+  oeForward(id, enquiry){
+    return   this.http.put("http://localhost:8080/enquiry/oeForward/"+id, enquiry)
+  }
+
+  oeGetEnquiries(){
+     return this.http.get("http://localhost:8080/enquiry/oeGetEnquiries")
+  }
+
 }
