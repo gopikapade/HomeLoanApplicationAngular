b74e3150718c83d124a3c4e98b1098eefd18c278
Bhushan <80579471+lurnfx@users.noreply.github.com>
2023-05-21 13:51:30 +0530

    update  Emi calculator

--- HomeLoanAppUI/src/app/login-module/templates/emicalculator/emicalculator.component.ts
+++ HomeLoanAppUI/src/app/login-module/templates/emicalculator/emicalculator.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Chart } from 'chart.js';
+
+
 
 @Component({
   selector: 'app-emicalculator',
@@ -7,4 +10,64 @@ import { Component } from '@angular/core';
 })
 export class EMIcalculatorComponent {
 
+
+  @ViewChild('loanChart') loanChartRef: ElementRef;
+  loanAmount: number;
+  loanTenure: number;
+  loanRate: number;
+  loanEmi: number;
+  loanPrinciple: number;
+  loanTotal: number;
+  loanInterest: number;
+
+  ngOnInit() {
+
+  }
+
+  calculateLoan() {
+    const amount = this.loanAmount;
+    const tenure = this.loanTenure * 12;
+    const rate = this.loanRate / 12 / 100;
+
+    const emi = (amount * rate * Math.pow(1 + rate, tenure)) / (Math.pow(1 + rate, tenure) - 1);
+    const total = emi * tenure;
+    const interest = total - amount;
+
+    this.loanEmi = Math.floor(emi);
+    this.loanPrinciple = Math.floor(amount);
+    this.loanTotal = Math.floor(total);
+    this.loanInterest = Math.floor(interest);
+
+    // Loan chart
+    const xValues = ['Principle', 'Interest'];
+    const yValues = [amount, Math.floor(interest)];
+    const barColors = ['#961251', '#000000'];
+
+    new Chart(this.loanChartRef.nativeElement, {
+      type: 'pie',
+      data: {
+        labels: xValues,
+        datasets: [
+          {
+            backgroundColor: barColors,
+            data: yValues
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: 'Chart.js Pie Chart'
+          }
+        }
+      }
+    });
+  }
 }
+
+
