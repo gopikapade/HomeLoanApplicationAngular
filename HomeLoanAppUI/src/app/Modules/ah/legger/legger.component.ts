0f2696c0d1956300691b0493d29df01862211d62
Bhushan <80579471+lurnfx@users.noreply.github.com>
2023-05-28 18:12:18 +0530

    Push array

--- HomeLoanAppUI/src/app/Modules/ah/legger/legger.component.ts
+++ HomeLoanAppUI/src/app/Modules/ah/legger/legger.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AhService } from 'src/app/service/ah.service';
 
 @Component({
   selector: 'app-legger',
@@ -8,5 +9,21 @@ import { Component } from '@angular/core';
 export class LeggerComponent {
 
   ledger:any
+
+  constructor(private ahservice:AhService){}
+  customers:any[]
+  leagers:any[]=[]
+  ngOnInit()
+  {
+    this.ahservice.getAllLoanAcounts().subscribe((data:any)=>{
+      this.customers =data.body
+      this.customers.forEach(element=>{
+       if(element.loandisbursement.loanNo!=null){
+         this.leagers.push(element)
+       }
+      })
+      console.log(this.leagers)
+    })
+  }
 
 }
